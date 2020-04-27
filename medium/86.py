81ce9ffa1bcab41f1dc9888a0ab7b3712f8c4123
cleeag <jack0506000@gmail.com>
2020-04-27 21:45:18 +0800

    done 86, 99.74%

---
+++ medium/86.py
@@ -0,0 +1,34 @@
+import os
+from os.path import join
+
+from src.list_node import ListNode
+
+def partition(head: ListNode, x):
+    if head is None: return
+    cutoff = ListNode(x - 1)
+    cutoff.next = head
+    prev = cutoff
+    p = head
+
+    while p :
+        if p.val < x and prev != cutoff:
+            tmp = p.next
+            prev.next = p.next
+            p.next = cutoff.next
+            cutoff.next = p
+            cutoff = p
+            p = tmp
+            if cutoff.next == head:
+                head = cutoff
+        elif p.val < x and prev == cutoff:
+            p = p.next
+            prev = prev.next
+            cutoff = prev
+        else:
+            p = p.next
+            prev = prev.next
+
+    return head
+
+if __name__ == '__main__':
+    pass
